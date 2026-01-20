d4a3f6e934d40527d71dacd4e9512e9f01934dfd
darkrain88 <39786175+darkrain88@users.noreply.github.com>
2026-01-20 09:05:34 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1177,9 +1177,9 @@ main() {
     fix_openssl_ktls
     fix_opkg_check
     update_package "runc" "releases" "v1.2.6"
-    update_package "containerd" "releases" "v1.7.27"
-    update_package "docker" "tags" "v28.2.2"
-    update_package "dockerd" "releases" "v28.2.2"
+    update_package "containerd" "releases" "v2.2.1"
+    update_package "docker" "tags" "v29.1.5"
+    update_package "dockerd" "releases" "v29.1.5"
 	update_package "softethervpn5"
 	update_package "zerotier"
     # apply_hash_fixes # 调用哈希修正函数
